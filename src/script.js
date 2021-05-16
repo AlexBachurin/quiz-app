170d7dd8dc0d03e416efaf88c6074bfaa2ffba14
Alex <alexbachurin91@gmail.com>
2021-05-17 02:53:59 +0300

    a bit of code refactor

--- src/script.js
+++ src/script.js
@@ -28,8 +28,10 @@ restartBtn.addEventListener('click', () => {
 let questionCounter = 0;
 //counter for correct answers
 let correctAns = 0;
-//timer setup
+//timer time in seconds
 let timerSec = 15;
+//quiz timer setinterval id
+let quizTimer;
 
 //function to show card info, getting data from array from questions.js
 function showCardInfo(index) {
@@ -41,38 +43,18 @@ function showCardInfo(index) {
     const options = item.options;
     const answer = item.answer;
     //show question
-    const questionDOM = document.querySelector('.quiz__question')
-    const queText = `${question}`
-    questionDOM.textContent = queText;
+    showQuestion(question);
 
     //show options
-    const optionsDOM = document.querySelector('.quiz__options')
-    let optionsList = '';
-    optionsList = options.map(option => {
-        if (option === answer) {
-            return `<div class="quiz__option">
-            <span>${option}</span>
-            <div class="quiz__icon quiz__icon_tick"><i class="fas fa-check"></i></div>
-        </div>`
-        } else {
-            return `<div class="quiz__option">
-        <span>${option}</span>
-        </div>`
-        }
-    }).join('')
-    optionsDOM.innerHTML = optionsList;
+    showOptionsList(answer, options)
 
     //setup footer numbers and disable next button
-    const currentOption = document.querySelector('.quiz__num-current');
-    const totalNumOption = document.querySelector('.quiz__num-total');
-    const nextBtn = document.querySelector('.quiz__btn-next');
-    currentOption.textContent = questionCounter + 1;
-    totalNumOption.textContent = questions.length;
-    nextBtn.disabled = true;
+    quizFooterInfo();
+    //disable btn
+    disableBtn();
 
     //setup clicking on option choice
     const optionChoice = [...document.querySelectorAll('.quiz__option')];
-    const optionTitle = [...document.querySelectorAll('.quiz__option span')];
     const icons = document.querySelectorAll('.quiz__icon');
     const wrongIcon = `<div class=" quiz__icon quiz__icon_cross"><i class="fas fa-times"></i></div>`
     
@@ -82,15 +64,9 @@ function showCardInfo(index) {
             const target = e.target;
             //get clicked element text content
             const text = target.textContent;
-            //find correct option
-            const correctOption = optionChoice.filter(item => {
-                const child = item.firstElementChild;
-                if (child.textContent === answer) {
-                    return item;
-                }
-            })
+            //get correct answer
+            const correctOption = findCorrectAnswer(optionChoice, answer);
 
-            console.log(correctOption);
             if (text === answer) {
                 //increase correct answer value and show green tick to user
                 correctAns++;
@@ -110,18 +86,81 @@ function showCardInfo(index) {
                 //show correct option
                 correctOption[0].classList.add('correct');
             }
-            //activate next button
-            nextBtn.disabled = false;
+            //enable next button
+            enableBtn();
             //disable clicking on options
             optionChoice.forEach(option => {
                 option.classList.add('answered')
             })
+            //clear quiz timer
+            clearInterval(quizTimer)
 
         })
         
         
     })
 }
+
+// **** SHOW QUIZ CARD INFO ****
+//show quiz question
+function showQuestion(question) {
+    const questionDOM = document.querySelector('.quiz__question')
+    const queText = `${question}`
+    questionDOM.textContent = queText;
+}
+
+//show options list
+function showOptionsList(answer, options) {
+    const optionsDOM = document.querySelector('.quiz__options')
+    let optionsList = '';
+    optionsList = options.map(option => {
+        if (option === answer) {
+            return `<div class="quiz__option">
+            <span>${option}</span>
+            <div class="quiz__icon quiz__icon_tick"><i class="fas fa-check"></i></div>
+        </div>`
+        } else {
+            return `<div class="quiz__option">
+        <span>${option}</span>
+        </div>`
+        }
+    }).join('')
+    optionsDOM.innerHTML = optionsList;
+}
+
+function quizFooterInfo() {
+    const currentOption = document.querySelector('.quiz__num-current');
+    const totalNumOption = document.querySelector('.quiz__num-total');
+    currentOption.textContent = questionCounter + 1;
+    totalNumOption.textContent = questions.length;
+}
+
+// **** HELPING FUNCTIONALITY ****
+//find correct option
+function findCorrectAnswer(optionsArr, correctAns) {
+    const correctOption = optionsArr.filter(item => {
+        const child = item.firstElementChild;
+        if (child.textContent === correctAns) {
+            return item;
+        }
+    })
+
+    return correctOption;
+}
+
+//disable button 
+function disableBtn() {
+    nextBtn.classList.add('btn-disabled');
+    nextBtn.disabled = true;
+}
+//enable button
+function enableBtn() {
+    nextBtn.classList.remove('btn-disabled');
+    nextBtn.disabled = false;
+}
+
+
+
 
 //next btn
 const nextBtn = document.querySelector('.quiz__btn-next');
@@ -160,9 +199,9 @@ function setupTimer(sec) {
         const timeDiff = getTimeDiff(deadline)
         timerDOM.textContent = timeDiff;
         if (timeDiff <= 0) {
-            clearInterval(timerId)
+            clearInterval(quizTimer)
         }
     }
 
-    let timerId = setInterval(updateTimer, 1000)
+    quizTimer = setInterval(updateTimer, 1000)
 }
