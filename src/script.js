6855ea3ab10f5477a62e1512cd225cd576269ad0
Alex <alexbachurin91@gmail.com>
2021-05-17 01:48:11 +0300

    showing correct and wrong options after click

--- src/script.js
+++ src/script.js
@@ -26,6 +26,8 @@ restartBtn.addEventListener('click', () => {
 
 //counter to count questions to not exceed length
 let questionCounter = 0;
+//counter for correct answers
+let correctAns = 0;
 
 //function to show card info, getting data from array from questions.js
 function showCardInfo(index) {
@@ -56,21 +58,64 @@ function showCardInfo(index) {
     }).join('')
     optionsDOM.innerHTML = optionsList;
 
-    //setup footer numbers
+    //setup footer numbers and disable next button
     const currentOption = document.querySelector('.quiz__num-current');
     const totalNumOption = document.querySelector('.quiz__num-total');
+    const nextBtn = document.querySelector('.quiz__btn-next');
     currentOption.textContent = questionCounter + 1;
     totalNumOption.textContent = questions.length;
+    nextBtn.disabled = true;
 
     //setup clicking on option choice
-    const optionChoice = document.querySelectorAll('.quiz__option');
+    const optionChoice = [...document.querySelectorAll('.quiz__option')];
+    const optionTitle = [...document.querySelectorAll('.quiz__option span')];
+    const icons = document.querySelectorAll('.quiz__icon');
+    const wrongIcon = `<div class=" quiz__icon quiz__icon_cross"><i class="fas fa-times"></i></div>`
+    
+    
     optionChoice.forEach(option => {
         option.addEventListener('click', (e) => {
             const target = e.target;
             //get clicked element text content
             const text = target.textContent;
-            console.log(text);
+            //find correct option
+            const correctOption = optionChoice.filter(item => {
+                const child = item.firstElementChild;
+                if (child.textContent === answer) {
+                    return item;
+                }
+            })
+
+            console.log(correctOption);
+            if (text === answer) {
+                //increase correct answer value and show green tick to user
+                correctAns++;
+                option.classList.add('correct')
+                icons.forEach(icon => {
+                    icon.classList.add('display-icon')
+                })
+            } else {
+                //else append cross icon to clicked element and show right answer
+                // and decorate wrong answer
+                option.innerHTML += wrongIcon;
+                option.classList.add('wrong');
+                //display icons
+                icons.forEach(icon => {
+                    icon.classList.add('display-icon')
+                })
+                //show correct option
+                correctOption[0].classList.add('correct');
+            }
+            //activate next button
+            nextBtn.disabled = false;
+            //disable clicking on options
+            optionChoice.forEach(option => {
+                option.classList.add('answered')
+            })
+
         })
+        
+        
     })
 }
 
